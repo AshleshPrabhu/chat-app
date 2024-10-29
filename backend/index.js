7e3a76febdcdcded2628b9fd76f6a3216d15d799
Ashlesh Prabhu <ashlesh.prabhu5@gmail.com>
2024-10-29 20:39:37 +0530

    set up router and change the backend file structure

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 import express from 'express'
 import dotenv  from 'dotenv';
-const app=express();
-dotenv.config();
-app.get("/",(req,res)=>{
-    res.send("Hello World");
-})
-const port = process.env.PORT||5000
-app.listen(port,console.log(`server started at port ${port}`))
+import { app } from '../backend/src/app.js';
+dotenv.config({
+    path: './.env',
+});
+app.on(
+    'error',
+    (err) => {
+        console.error('Error:', err);
+        }
+)
+app.listen(process.env.PORT||8000,()=>{
+    console.log(`server is running at port ${process.env.PORT}`)
+});
 
+ 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,25 @@ import {Provider} from "./components/ui/provider.jsx"
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {  Route, RouterProvider, createBrowserRouter, createRoutesFromElements} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Chats from './pages/Chats.jsx'
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+      <Route path='' element={<Home/>}/>
+      <Route path='/chats' element={<Chats/>}/>
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider>
-      <App /> 
+      <RouterProvider
+        router={router}
+      
+      />
     </Provider>
   </StrictMode>,
 )
