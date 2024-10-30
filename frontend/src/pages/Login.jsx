7e64b9bb7d2fe6bb4a38866127670f686e912f31
Ashlesh Prabhu <ashlesh.prabhu5@gmail.com>
2024-10-30 22:28:17 +0530

    make changes to login and signup

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,13 @@
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import React from 'react'
+import { Button } from '@/components/ui/button'
+
+import React, { useState } from 'react'
 
 function Login() {
+    const [email, setEmail] = useState(null)
+    const [password, setPassword] = useState(null)
+
   return (
     <div className='w-full flex items-center justify-center'>
         <form className='w-[90%]'>
@@ -10,21 +15,35 @@ function Login() {
                 <Label htmlFor='Email'>
                     Email
                 </Label>
-                <Input id='Email' placeholder='Enter your Email' type='email' className='w-[90%]'/>
+                <Input 
+                    id='Email' 
+                    placeholder='Enter your Email' 
+                    type='email' 
+                    className='w-[90%]'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
             </div>
             <div className='mb-7'>
                 <Label htmlFor='Password'>
                     Password
                 </Label>
-                <Input id='Password' placeholder='Enter Password' type='password' className='w-[90%]'/>
+                <Input 
+                    id='Password' 
+                    placeholder='Enter Password' 
+                    type='password' 
+                    className='w-[90%]'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}   
+                />
             </div>
             <div className='w-full flex items-center justify-center'>
-                <Button className='rounded-lg w-10 bg-blue-400 text-white'> 
+                <Button className='rounded-lg w-[70%] mb-2 bg-blue-400 text-white'> 
                     Login
                 </Button>
             </div>
             <div className='w-full flex items-center justify-center'>
-                <Button className='rounded-lg w-10 bg-orange-300 text-white'> 
+                <Button className='rounded-lg w-[70%] mb-5 bg-orange-300 text-white'> 
                     Get Guest User Credentials
                 </Button>
             </div>
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,9 +1,14 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import React from 'react'
+import React,{useState} from 'react'
 
 function Signup() {
+    const [name, setName] = useState(null)
+    const [email, setEmail] = useState(null)
+    const [password, setPassword] = useState(null)
+    const [confirmPassword, setConfirmPassword] = useState(null)
+    const [pic, setPic] = useState(null)
   return (
     <div className='w-full flex items-center justify-center'>
         <form className='w-[90%]'>
@@ -11,35 +16,70 @@ function Signup() {
                 <Label htmlFor='Name'>
                     Name
                 </Label>
-                <Input id='Name' placeholder='Enter your Name' className='w-[90%]'/>
+                <Input 
+                    id='Name' 
+                    placeholder='Enter your Name' 
+                    className='w-[90%]'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
             </div>
             
             <div className='mb-7'>
                 <Label htmlFor='Email'>
                     Email
                 </Label>
-                <Input id='Email' placeholder='Enter your Email' type='email' className='w-[90%]'/>
+                <Input 
+                    id='Email' 
+                    placeholder='Enter your Email' 
+                    type='email' 
+                    className='w-[90%]'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                />
             </div>
             <div className='mb-7'>
                 <Label htmlFor='Password'>
                     Password
                 </Label>
-                <Input id='Password' placeholder='Enter Password' type='password' className='w-[90%]'/>
+                <Input 
+                    id='Password' 
+                    placeholder='Enter Password' 
+                    type='password' 
+                    className='w-[90%]'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
             </div>
+            //TODO:  make show password functionality
             <div className='mb-7'>
                 <Label htmlFor='ConfirmPassword'>
                     Confirm Password
                 </Label>
-                <Input id='ConfirmPassword' placeholder='Confirm Password' type='password' className='w-[90%]'/>
+                <Input 
+                    id='ConfirmPassword' 
+                    placeholder='Confirm Password' 
+                    type='password' 
+                    className='w-[90%]'
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                />
             </div>
             <div className='mb-7'>
                 <Label htmlFor='profileimg'>
                     Upload your Picture
                 </Label>
-                <Input id='profileimg' placeholder='Confirm Password' type='file' className='w-[90%]'/>
+                <Input 
+                    id='profileimg' 
+                    placeholder='Confirm Password' 
+                    type='file' 
+                    className='w-[90%]'
+                    value={pic}
+                    onChange={(e) => setPic(e.target.files[0]||"")}
+                />
             </div>
             <div className='w-full flex items-center justify-center'>
-                <Button className='rounded-lg w-10 bg-blue-400 text-white'> 
+                <Button className='rounded-xl w-[70%] mb-5 bg-blue-400 text-white'> 
                     Signup
                 </Button>
             </div>
